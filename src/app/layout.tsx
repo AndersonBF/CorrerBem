8870b26e4604162e1459e2f14d48b4753014fdf5
Anderson Fernandes <95195152+AndersonBF@users.noreply.github.com>
2025-08-26 09:34:46 -0300

    googletag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AosInit } from "./_components/aos-init";
 import { Bebas_Neue } from "next/font/google";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,16 +28,44 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="pt-br">
+      <head>
+        {/* Google Tag Manager */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id=GTM-NTK5TZ73'+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-NTK5TZ73');
+            `,
+          }}
+        />
+        {/* End Google Tag Manager */}
+      </head>
       <body
-        className={`${bebasNeue.variable} ${geistSans.variable} ${geistMono.variable}  antialiased`}
+        className={`${bebasNeue.variable} ${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NTK5TZ73"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
         {children}
-         <AosInit />
+        <AosInit />
       </body>
     </html>
   );
